54f383c4bfe3389d4bda442a4c4be615c927529a
Matheus Santos <matheus.santos@benner.com.br>
2019-07-16 13:46:29 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoBiblioteca/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoBiblioteca.MvcApplication" Language="C#" %>
---
+++ ProjetoBiblioteca/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoBiblioteca.Startup))]
+namespace ProjetoBiblioteca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
