460dcb956d35d368c8339ff122c37fe6af7c643b
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 20:47:36 -0300

    Classe Endereco e algumas propriedades

---
+++ BibliotecaModelos/Endereco.cs
@@ -0,0 +1,9 @@
+﻿namespace BibliotecaModelos
+{
+    public class Endereco
+    {
+        public string CEP { get; private set; }
+        public string Logradouro { get; private set; }
+        public Municipio Municipio { get; private set; }
+    }
+}
