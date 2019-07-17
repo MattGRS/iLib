0079385d4daa3eab9b6ef82525563b05ec198069
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 20:46:32 -0300

    Classe pessoa e algumas propriedades

---
+++ BibliotecaModelos/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModelos
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public Endereco Endereco { get; privateset; }
+    }
+}
