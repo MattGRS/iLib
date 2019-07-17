e0c7a3f3741184386b38aacb2ccc368ccee31e41
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 20:49:59 -0300

    Prop IdPessoa em Endereco

--- BibliotecaModelos/Endereco.cs
+++ BibliotecaModelos/Endereco.cs
@@ -2,6 +2,8 @@
 {
     public class Endereco
     {
+        //Confirmar propriedade IdPessoa                          ´
+        public int IdPessoa { get; set; }
         public string CEP { get; private set; }
         public string Logradouro { get; private set; }
         public Municipio Municipio { get; private set; }
