88518bc6c404f53ab5056d2592e1412064ad3db5
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 21:13:49 -0300

    Classe Editora e prop

---
+++ BibliotecaModelos/Editora.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaModelos
+{
+    public class Editora
+    {
+        public string NomeEditora { get; private set; }
+        public Endereco Endereco { get; private set; }
+    }
+}
