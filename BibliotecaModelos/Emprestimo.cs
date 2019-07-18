7154fcdb624c7e26cdb0e2d2708fe8d642a6fc78
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 21:24:31 -0300

    Classe Emprestimo e prop

---
+++ BibliotecaModelos/Emprestimo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModelos
+{
+    public class Emprestimo
+    {
+        public DateTime DataEmprestimo { get; private set; }
+        public DateTime DataDevolucao { get; private set; }
+    }
+}
