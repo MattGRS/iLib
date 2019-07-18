899d2fd7ddae73aa6347c1e7886ee3d9943743fd
Matheus Santos <matheus.santos@benner.com.br>
2019-07-17 21:10:54 -0300

    Classe Livro e algumas propriedades

---
+++ BibliotecaModelos/Livro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaModelos
+{
+    public class Livro
+    {
+        public string Titulo { get; private set; }
+        public Autor Autor { get; private set; }
+        public Editora Editora { get; private set; }
+        public Assunto Assunto { get; private set; }
+        public Classificacao Classificacao { get; private set; }
+    }
+}
