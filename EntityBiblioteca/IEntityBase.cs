0b24f5ca8ddb79c36d828013335c2261a257c36d
Matheus Santos <matheus.santos@benner.com.br>
2019-07-18 22:23:27 -0300

    Interface Base de CRUD

---
+++ EntityBiblioteca/IEntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityBiblioteca
+{
+    interface IEntityBase<T> where T : class
+    {
+        void Adicionar(T entidade);
+        void Atualizar(T entidade);
+        void Remover(T entidade);
+        //IList<T> Lista();
+    }
+}
