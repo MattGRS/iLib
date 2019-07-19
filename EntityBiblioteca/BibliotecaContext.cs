410abb19be80ceb595d92299b7794e164463f0ff
Matheus Santos <matheus.santos@benner.com.br>
2019-07-18 21:50:14 -0300

    Construtores e Prop OnConfiguring

---
+++ EntityBiblioteca/BibliotecaContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityBiblioteca
+{
+    internal class BibliotecaContext :DbContext
+    {
+        public BibliotecaContext()
+        {
+
+        }
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BibliotecaDB;Trusted_Connection=true;");
+            }
+        }
+    }
+}
