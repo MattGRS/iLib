e2f38bbfb8508614e51fbdfa4b82db43c56030bf
Matheus Santos <matheus.santos@benner.com.br>
2019-08-05 20:50:42 -0300

    ExemplarConfig

---
+++ BibliotecaDados/EntidadesConfig/ExemplarConfig.cs
@@ -0,0 +1,24 @@
+﻿using BibliotecaDominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDados.EntidadesConfig
+{
+    class ExemplarConfig : IEntityTypeConfiguration<ExemplarLivro>
+    {
+        public void Configure(EntityTypeBuilder<ExemplarLivro> builder)
+        {
+            builder
+                .Property(e => e.Registro)
+                .IsRequired();
+            builder
+                .Property(e => e.Livro)
+                .IsRequired();
+        }
+    }
+}
