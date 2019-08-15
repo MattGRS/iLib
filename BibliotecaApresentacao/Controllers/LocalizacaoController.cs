22124b0ae1f69b13cf213d853026d94de38de6f8
Matheus Santos <matheus.santos@benner.com.br>
2019-08-15 17:46:23 -0300

    Pais CRUD completo

---
+++ BibliotecaApresentacao/Controllers/LocalizacaoController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using BibliotecaAplicacao.Interfaces;
+using BibliotecaApresentacao.ViewModels;
+using BibliotecaDominio.Entidades.ObjetosValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaApresentacao.Controllers
+{
+    public class LocalizacaoController : Controller
+    {
+        private readonly ILocalizacaoAppServico _localizacaoAppServico;
+        public LocalizacaoController(ILocalizacaoAppServico localizacaoAppServico)
+        {
+            _localizacaoAppServico = localizacaoAppServico;
+        }
+
+        public ActionResult Index()
+        {
+            var localizacaoViewModel = Mapper.Map<IEnumerable<Localizacao>, IEnumerable<LocalizacaoViewModel>>(_localizacaoAppServico.ObterTodos());
+            return View(localizacaoViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LocalizacaoViewModel localizacaoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var localizacaoEntidade = Mapper.Map<LocalizacaoViewModel, Localizacao>(localizacaoViewModel);
+                _localizacaoAppServico.Adicionar(localizacaoEntidade);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(localizacaoViewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var localizacaoEntidade = _localizacaoAppServico.ObterPorId(id);
+            if (_localizacaoAppServico.Remover(localizacaoEntidade))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Erro", new { msg = $"O Iten {localizacaoEntidade.LocalizacaoObra} não pode ser removido pois existe um livro vinculado" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var localizacaoEntidade = _localizacaoAppServico.ObterPorId(id);
+            var localizacaoViewModel = Mapper.Map<Localizacao, LocalizacaoViewModel>(localizacaoEntidade);
+            ViewBag.Localizacao = localizacaoViewModel;
+            return View(localizacaoViewModel);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, LocalizacaoViewModel localizacaoViewModel)
+        {
+            localizacaoViewModel.LocalizacaoId = id;
+            var localizacaoEntidade = Mapper.Map<LocalizacaoViewModel, Localizacao>(localizacaoViewModel);
+            _localizacaoAppServico.Atualizar(localizacaoEntidade);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BibliotecaApresentacao/Controllers/PaisController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using BibliotecaAplicacao.Interfaces;
+using BibliotecaApresentacao.ViewModels;
+using BibliotecaDominio.Entidades.ObjetosValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaApresentacao.Controllers
+{
+    public class PaisController : Controller
+    {
+        private readonly IPaisAppServico _paisAppServico;
+        public PaisController(IPaisAppServico paisAppServico)
+        {
+            _paisAppServico = paisAppServico;
+        }
+
+        public ActionResult Index()
+        {
+            var paisViewModel = Mapper.Map<IEnumerable<Pais>, IEnumerable<PaisViewModel>>(_paisAppServico.ObterTodos());
+            return View(paisViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(PaisViewModel paisViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var paisEntidade = Mapper.Map<PaisViewModel, Pais>(paisViewModel);
+                _paisAppServico.Adicionar(paisEntidade);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(paisViewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var paisEntidade = _paisAppServico.ObterPorId(id);
+            if (_paisAppServico.Remover(paisEntidade))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Erro", new { msg = $"O Iten {paisEntidade.NomePais} não pode ser removido pois existe um estado vinculado" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var paisEntidade = _paisAppServico.ObterPorId(id);
+            var paisViewModel = Mapper.Map<Pais, PaisViewModel>(paisEntidade);
+            ViewBag.Pais = paisViewModel;
+            return View(paisViewModel);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, PaisViewModel paisViewModel)
+        {
+            paisViewModel.PaisId = id;
+            var paisEntidade = Mapper.Map<PaisViewModel, Pais>(paisViewModel);
+            _paisAppServico.Atualizar(paisEntidade);
+            return RedirectToAction("Index");
+        }
+    }
+}
