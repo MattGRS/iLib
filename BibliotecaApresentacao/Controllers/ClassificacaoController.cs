9d8c9b43f0d69de16ac9ed0032a95381cc4dbec9
Matheus Santos <matheus.santos@benner.com.br>
2019-08-15 17:12:32 -0300

    Classificacao CRUD completo

---
+++ BibliotecaApresentacao/Controllers/ClassificacaoController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using BibliotecaAplicacao.Interfaces;
+using BibliotecaApresentacao.ViewModels;
+using BibliotecaDominio.Entidades.ObjetosValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaApresentacao.Controllers
+{
+    public class ClassificacaoController : Controller
+    {
+        private readonly IClassificacaoAppServico _classificacaoAppServico;
+        public ClassificacaoController(IClassificacaoAppServico classificacaoAppServico)
+        {
+            _classificacaoAppServico = classificacaoAppServico;
+        }
+
+        public ActionResult Index()
+        {
+            var classificacaoViewModel = Mapper.Map<IEnumerable<Classificacao>, IEnumerable<ClassificacaoViewModel>>(_classificacaoAppServico.ObterTodos());
+            return View(classificacaoViewModel);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ClassificacaoViewModel classificacaoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var classificacaoEntidade = Mapper.Map<ClassificacaoViewModel, Classificacao>(classificacaoViewModel);
+                _classificacaoAppServico.Adicionar(classificacaoEntidade);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(classificacaoViewModel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var classificacaoEntidade = _classificacaoAppServico.ObterPorId(id);
+            if (_classificacaoAppServico.Remover(classificacaoEntidade))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index", "Erro", new { msg = $"O Iten {classificacaoEntidade.ClassificacaoObra} não pode ser removido pois existe um livro vinculado" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var classificacaoEntidade = _classificacaoAppServico.ObterPorId(id);
+            var classificacaoViewModel = Mapper.Map<Classificacao, ClassificacaoViewModel>(classificacaoEntidade);
+            ViewBag.Classificacao = classificacaoViewModel;
+            return View(classificacaoViewModel);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, ClassificacaoViewModel classificacaoViewModel)
+        {
+            classificacaoViewModel.ClassificacaoId = id;
+            var classificacaoEntidade = Mapper.Map<ClassificacaoViewModel, Classificacao>(classificacaoViewModel);
+            _classificacaoAppServico.Atualizar(classificacaoEntidade);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BibliotecaApresentacao/ViewModels/ClassificacaoViewModel.cs
+++ BibliotecaApresentacao/ViewModels/ClassificacaoViewModel.cs
@@ -12,7 +12,7 @@ namespace BibliotecaApresentacao.ViewModels
         public int ClassificacaoId { get; set; }
         [Required(ErrorMessage = "Campo Classificação é obrigatório.")]
         [MaxLength(100, ErrorMessage = "Máximo {0} caracteres.")]
-        public string ClassificacaoObra { get; internal set; }
+        public string ClassificacaoObra { get; set; }
         public virtual IEnumerable<LivroViewModel> Livros { get; internal set; }
     }
 }
